8ee5e649becf3b2b0419199d88c424714589acfa
AhmedSamy02 <https://github.com/AhmedSamy02>
2024-03-14 06:01:55 +0200

    [🎨 REF] Adding Bloc Package

---
+++ lib/cubits/product_details/product_details_states.dart
@@ -0,0 +1,9 @@
+class ProductDetailsStates {}
+
+class ProductDetailsInitialState extends ProductDetailsStates {}
+
+class ProductDetailsLoadingState extends ProductDetailsStates {}
+
+class ProductDetailsSuccessState extends ProductDetailsStates {}
+
+class ProductDetailsErrorState extends ProductDetailsStates {}
