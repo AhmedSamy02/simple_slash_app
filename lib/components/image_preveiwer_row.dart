82639fcc7cbc8c2e263b47ae2f6bd201909fa345
AhmedSamy02 <https://github.com/AhmedSamy02>
2024-03-16 03:36:25 +0200

    [🎨 REF] Adding FullScreenPreview

--- lib/components/image_preveiwer_row.dart
+++ lib/components/image_preveiwer_row.dart
@@ -2,9 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:full_screen_image/full_screen_image.dart';
 import 'package:simple_slash_app/constants.dart';
 
-class ImagesPreviewerRow extends StatefulWidget {
+class ImagesPreviewerRow extends StatelessWidget {
   const ImagesPreviewerRow({
     super.key,
     required this.swiperController,
@@ -15,14 +16,9 @@ class ImagesPreviewerRow extends StatefulWidget {
   final List<String> images;
 
   @override
-  State<ImagesPreviewerRow> createState() => _ImagesPreviewerRowState();
-}
-
-class _ImagesPreviewerRowState extends State<ImagesPreviewerRow> {
-  @override
   Widget build(BuildContext context) {
     return Swiper(
-      controller: widget.swiperController,
+      controller: swiperController,
       outer: false,
       layout: SwiperLayout.CUSTOM,
       containerHeight: MediaQuery.of(context).size.height * 0.3,
@@ -40,28 +36,36 @@ class _ImagesPreviewerRowState extends State<ImagesPreviewerRow> {
       itemWidth: MediaQuery.of(context).size.width * 0.7,
       itemHeight: MediaQuery.of(context).size.height * 0.315,
       itemBuilder: (context, index) {
-        return Card(
-          color: Colors.white,
-          elevation: 18,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: CachedNetworkImage(
-              imageUrl: widget.images[index],
-              errorWidget: (context, url, error) {
-                return Image.asset(
-                  kBrandLogo,
-                  fit: BoxFit.fill,
-                );
-              },
-              progressIndicatorBuilder: (context, url, progress) {
-                return const Center(
-                  child: SpinKitFadingCube(
-                    color: Colors.white,
-                    size: 20,
+        return SafeArea(
+          child: FullScreenWidget(
+            disposeLevel: DisposeLevel.Low,
+            child: Hero(
+              tag: 'smallImage',
+              child: Card(
+                color: Colors.white,
+                elevation: 18,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: CachedNetworkImage(
+                    imageUrl: images[index],
+                    errorWidget: (context, url, error) {
+                      return Image.asset(
+                        kBrandLogo,
+                        fit: BoxFit.fill,
+                      );
+                    },
+                    progressIndicatorBuilder: (context, url, progress) {
+                      return const Center(
+                        child: SpinKitFadingCube(
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      );
+                    },
+                    filterQuality: FilterQuality.none,
                   ),
-                );
-              },
-              filterQuality: FilterQuality.none,
+                ),
+              ),
             ),
           ),
         );
@@ -71,21 +75,20 @@ class _ImagesPreviewerRowState extends State<ImagesPreviewerRow> {
           return Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.325,
+                height: MediaQuery.of(context).size.height * 0.31,
               ),
               SizedBox(
                 width: double.infinity,
                 height: MediaQuery.of(context).size.height * 0.1,
                 child: Center(
                   child: ListView.builder(
-                    itemCount: widget.images.length,
+                    itemCount: images.length,
                     shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
                       return InkWell(
                         onTap: () {
-
-                          widget.swiperController.move(index);
+                          swiperController.move(index);
                         },
                         child: Container(
                           margin: const EdgeInsets.all(14),
@@ -102,7 +105,7 @@ class _ImagesPreviewerRowState extends State<ImagesPreviewerRow> {
                             borderRadius: BorderRadius.circular(12),
                             clipBehavior: Clip.antiAlias,
                             child: CachedNetworkImage(
-                              imageUrl: widget.images[index],
+                              imageUrl: images[index],
                               errorWidget: (context, url, error) {
                                 return Image.asset(
                                   kBrandLogo,
@@ -133,7 +136,7 @@ class _ImagesPreviewerRowState extends State<ImagesPreviewerRow> {
         },
       ),
       curve: Curves.linear,
-      itemCount: widget.images.length,
+      itemCount: images.length,
       loop: false,
     );
   }
